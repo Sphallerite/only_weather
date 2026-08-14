65812666c59abcfe1eae615cad2fd153329ed21f
Sphallerite <johnnyhoran0@gmail.com>
2026-08-14 01:10:22 -0400

    add glow box mod icons mod buttons

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const unit_button = document.getElementById("unit-button");
+
+unit_button.addEventListener("click", () => {
+  const text = unit_button.querySelector(".button-text");
+  if (text.textContent == "°F") {
+    text.textContent = "°C";
+  } else {
+    text.textContent = "°F";
+  }
+});
