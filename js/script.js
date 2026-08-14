287363726f5c0b45bbeec5cdc6e0b42eceec5c9f
Sphallerite <johnnyhoran0@gmail.com>
2026-08-14 15:14:34 -0400

    add feels like, percipitation, dew point, wind

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ const unit_button = document.getElementById("unit-button");
 
 unit_button.addEventListener("click", () => {
   const text = unit_button.querySelector(".button-text");
-  if (text.textContent == "°F") {
-    text.textContent = "°C";
+  if (text.textContent == "F") {
+    text.textContent = "C";
   } else {
-    text.textContent = "°F";
+    text.textContent = "F";
   }
 });
